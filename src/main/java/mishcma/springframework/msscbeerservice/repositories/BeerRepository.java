0311afd8552d184d51759043bd18e5ea86a99c1e
Maksym_Mishchenko <maksimus2998@gmail.com>
2022-08-19 11:59:05 +0200

    added beer repository

---
+++ src/main/java/mishcma/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,10 @@
+package mishcma.springframework.msscbeerservice.repositories;
+
+import mishcma.springframework.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+}
