58d62c16149070cb9a3ace596008b648e9e812ad
Maksym_Mishchenko <maksimus2998@gmail.com>
2022-08-25 15:30:22 +0200

    added ehcache

--- src/main/java/mishcma/springframework/msscbeerservice/services/BeerServiceImpl.java
+++ src/main/java/mishcma/springframework/msscbeerservice/services/BeerServiceImpl.java
@@ -7,6 +7,7 @@ import mishcma.springframework.msscbeerservice.web.mappers.BeerMapper;
 import mishcma.springframework.msscbeerservice.web.model.BeerDto;
 import mishcma.springframework.msscbeerservice.web.model.BeerPagedList;
 import mishcma.springframework.msscbeerservice.web.model.BeerStyleEnum;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,7 @@ public class BeerServiceImpl implements BeerService {
     private final BeerRepository beerRepository;
     private final BeerMapper beerMapper;
 
+    @Cacheable(cacheNames = "beerListCache", condition = "#showInventoryOnHand == false ")
     @Override
     public BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest, Boolean showInventoryOnHand) {
 
@@ -68,8 +70,11 @@ public class BeerServiceImpl implements BeerService {
 
         return beerPagedList;
     }
+
+    @Cacheable(cacheNames = "beerCache", key = "#beerId", condition = "#showInventoryOnHand == false")
     @Override
     public BeerDto getById(UUID beerId, Boolean showInventoryOnHand) {
+
         if (showInventoryOnHand) {
             return beerMapper.beerToBeerDtoWithInventory(
                     beerRepository.findById(beerId).orElseThrow(NotFoundException::new)
